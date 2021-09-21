3eb76282d454c8f37ccb9671d0380243883f698b
Christopher Dinh <cqdinh153@gmail.com>
2021-09-20 20:15:20 -0700

    Initial commit

---
+++ test/aws budget emergency cutoff.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as AwsBudgetEmergencyCutoff from '../lib/aws budget emergency cutoff-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new AwsBudgetEmergencyCutoff.AwsBudgetEmergencyCutoffStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
